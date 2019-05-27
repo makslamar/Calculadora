675b7b76ee984d1cd1a0d65c18df8ae0886ee748
makslamar <39013853+makslamar@users.noreply.github.com>
2019-05-27 18:58:42 -0500

    Interactivity Buttons
    changing the size of buttons

--- js/app.js
+++ js/app.js
@@ -1,4 +1,159 @@
-var calculadora = function(){
+var calculadora = {
+  inicializar: function(){
+    //** declaro variables **//
+     var uno = document.getElementById("1");
+     var dos = document.getElementById("2");
+     var tres = document.getElementById("3");
+     var cuatro = document.getElementById("4");
+     var cinco = document.getElementById("5");
+     var seis = document.getElementById("6");
+     var siete = document.getElementById("7");
+     var ocho = document.getElementById("8");
+     var nueve = document.getElementById("9");
+     var cero = document.getElementById("0");
+     var mas = document.getElementById("mas");
+     var menos = document.getElementById("menos");
+     var por = document.getElementById("por");
+     var dividido = document.getElementById("dividido");
+     var punto = document.getElementById("punto");
+     var igual = document.getElementById("igual");
+     var on = document.getElementById("on");
+     var sign = document.getElementById("sign");
+     var raiz = document.getElementById("raiz");
+    //**Interactividad de los botones - Cambio de tamaño**//
+    uno.addEventListener("mousedown",function(){
+    uno.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    uno.addEventListener("mouseup",function(){
+    uno.setAttribute("style","transform:scale(1,1)")
+    });
 
+    dos.addEventListener("mousedown",function(){
+    dos.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    dos.addEventListener("mouseup",function(){
+    dos.setAttribute("style","transform:scale(1,1)")
+    });
+
+    tres.addEventListener("mousedown",function(){
+    tres.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    tres.addEventListener("mouseup",function(){
+    tres.setAttribute("style","transform:scale(1,1)")
+    });
+
+    cuatro.addEventListener("mousedown",function(){
+    cuatro.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    cuatro.addEventListener("mouseup",function(){
+    cuatro.setAttribute("style","transform:scale(1,1)")
+    });
+
+    cinco.addEventListener("mousedown",function(){
+    cinco.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    cinco.addEventListener("mouseup",function(){
+    cinco.setAttribute("style","transform:scale(1,1)")
+    });
+
+    seis.addEventListener("mousedown",function(){
+    seis.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    seis.addEventListener("mouseup",function(){
+    seis.setAttribute("style","transform:scale(1,1)")
+    });
+
+    siete.addEventListener("mousedown",function(){
+    siete.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    siete.addEventListener("mouseup",function(){
+    siete.setAttribute("style","transform:scale(1,1)")
+    });
+
+    ocho.addEventListener("mousedown",function(){
+    ocho.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    ocho.addEventListener("mouseup",function(){
+    ocho.setAttribute("style","transform:scale(1,1)")
+    });
+
+    nueve.addEventListener("mousedown",function(){
+    nueve.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    nueve.addEventListener("mouseup",function(){
+    nueve.setAttribute("style","transform:scale(1,1)")
+    });
+
+    cero.addEventListener("mousedown",function(){
+    cero.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    cero.addEventListener("mouseup",function(){
+    cero.setAttribute("style","transform:scale(1,1)")
+    });
+
+    mas.addEventListener("mousedown",function(){
+    mas.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    mas.addEventListener("mouseup",function(){
+    mas.setAttribute("style","transform:scale(1,1)")
+    });
+
+    menos.addEventListener("mousedown",function(){
+    menos.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    menos.addEventListener("mouseup",function(){
+    menos.setAttribute("style","transform:scale(1,1)")
+    });
+
+    por.addEventListener("mousedown",function(){
+    por.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    por.addEventListener("mouseup",function(){
+    por.setAttribute("style","transform:scale(1,1)")
+    });
+
+    dividido.addEventListener("mousedown",function(){
+    dividido.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    dividido.addEventListener("mouseup",function(){
+    dividido.setAttribute("style","transform:scale(1,1)")
+    });
+
+    punto.addEventListener("mousedown",function(){
+    punto.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    punto.addEventListener("mouseup",function(){
+    punto.setAttribute("style","transform:scale(1,1)")
+    });
+
+    igual.addEventListener("mousedown",function(){
+    igual.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    igual.addEventListener("mouseup",function(){
+    igual.setAttribute("style","transform:scale(1,1)")
+    });
+
+    on.addEventListener("mousedown",function(){
+    on.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    on.addEventListener("mouseup",function(){
+    on.setAttribute("style","transform:scale(1,1)")
+    });
+
+    sign.addEventListener("mousedown",function(){
+    sign.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    sign.addEventListener("mouseup",function(){
+    sign.setAttribute("style","transform:scale(1,1)")
+    });
+
+    raiz.addEventListener("mousedown",function(){
+    raiz.setAttribute("style","transform:scale(0.85,0.85)")
+    });
+    raiz.addEventListener("mouseup",function(){
+    raiz.setAttribute("style","transform:scale(1,1)")
+    });
+
+  }
 };
-calculadora();
+calculadora.inicializar();
